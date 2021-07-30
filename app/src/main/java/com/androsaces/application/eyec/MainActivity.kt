b77a8fdb9be35c22d468ab03dfcf71185af84930
Andrew Kearney <andrew.kearney@bidfx.com>
2021-07-30 16:46:30 +1000

    Create basic Android app skeleton
    Resolves: IC-2

---
+++ app/src/main/java/com/androsaces/application/eyec/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androsaces.application.eyec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
